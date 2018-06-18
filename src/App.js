4db477794e1b942243c7932712e05d9f671716f9
rayGreycloud <rayGreycloud@gmail.com>
2018-06-17 17:10:54 -0700

    Change app to replace react logo with dog pic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const { fetching, dog, onRequestDog, error } = this.props;
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={dog || logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Dog Saga</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        {dog ? (
+          <p className="App-intro">Keep clicking for new dogs</p>
+        ) : (
+          <p className="App-intro">Replace the React icon with a dog!</p>
+        )}
+
+        {fetching ? (
+          <button disabled>Fetching...</button>
+        ) : (
+          <button onClick={onRequestDog}>Request a Dog</button>
+        )}
+
+        {error && <p style={{ color: 'red' }}>Uh oh - something went wrong!</p>}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  fetching: state.fetching,
+  dog: state.dog,
+  error: state.error
+});
+
+const mapDispatchToProps = dispatch => ({
+  onRequestDog: () => dispatch({ type: 'API_CALL_REQUEST' })
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
